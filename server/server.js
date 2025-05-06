34c7b5626e2ca219b6a9c5f1ed894661baf6ea2a
Drishti <drishti458386@gmail.com>
2025-05-06 16:40:15 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-app.use(cors());
+app.use(cors({ origin: 'https://guardian-of-wildlife.onrender.com' }));
 app.use(wildliferoute);
 app.use(programsroute);
 app.use(blogroute);
