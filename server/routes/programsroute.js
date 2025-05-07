6154a0b901e47207f98f45bc701d8c5231d0f3d9
Drishti <drishti458386@gmail.com>
2025-05-07 13:10:07 +0530

    first commit

--- server/routes/programsroute.js
+++ server/routes/programsroute.js
@@ -12,4 +12,18 @@ programsroute.get("/programs", async (req, res) => {
     }
 })
 
+// DELETE a program by ID
+programsroute.delete("/program/:num", async (req, res) => {
+    const { num } = req.params;
+    try {
+        const deletedProgram = await Programs.findByIdAndDelete(num);
+        if (!deletedProgram) {
+            return res.status(404).json({ message: "Program not found" });
+        }
+        res.status(200).json({ message: `${deletedProgram.name} deleted Successfully` });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = programsroute;
